f307bbc660cf724461e18b9307f803a4a8a70f21
nakamura <shop@goosys.sakura.ne.jp>
2025-03-19 12:55:10 +0000

    feat: Change ransack_options to helper

--- lib/administrate_ransack/helpers.rb
+++ lib/administrate_ransack/helpers.rb
@@ -3,6 +3,7 @@
 module AdministrateRansack
   class << self
     def ransack?(model, params = {}, options = {})
+      options.merge!(ignore_unknown_conditions: true)
       ransack = model.ransack(params, **options)
       ransack.instance_variable_get(:@scope_args).present? || ransack.base.c.present?
     end
