0d01f939ef31cea93d3424540578303bd71aa2a4
nakamura <shop@goosys.sakura.ne.jp>
2026-02-01 11:12:30 +0000

    fix: Add RansackCastArrayRevertPatch for Ransack 4.4.0

--- config/initializers/ransack.rb
+++ config/initializers/ransack.rb
@@ -6,3 +6,14 @@ Ransack.configure do |config|
     formatter: proc { |v| v.end_of_day },
     type: :date
 end
+
+if Gem.loaded_specs["ransack"]&.version.to_s == "4.4.0"
+  module RansackCastArrayRevertPatch
+    # Ransack 4.4.0 had a bug, and a partial revert was released in 4.4.1
+    # ref: https://github.com/activerecord-hackery/ransack/pull/1645
+    def casted_values_for_attribute(attr)
+      validated_values.map { |v| v.cast(predicate.type || attr.type) }
+    end
+  end
+  Ransack::Nodes::Condition.prepend(RansackCastArrayRevertPatch)
+end
