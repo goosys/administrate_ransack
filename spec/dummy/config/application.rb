5d78d921b997b95243e0a0e88d3aa4a320e21d50
nakamura <shop@goosys.sakura.ne.jp>
2026-02-01 08:34:58 +0000

    add load_defaults option to dummy app

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -10,13 +10,12 @@ require_relative '../lib/administrate/field/age'
 module Dummy
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    # config.load_defaults 6.0
+    config.load_defaults "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.active_support.cache_format_version = 7.0
 
     config.after_initialize do
       AdministrateRansack.add_filter('Administrate::Field::Age', 'field_age')
